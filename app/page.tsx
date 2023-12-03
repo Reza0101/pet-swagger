f2796b57fed5006f61dc90a3b7c729fe525e3397
RezaSiahi <rezasiahi8@gmail.com>
2023-12-03 08:11:31 +0000

    fix: my test message

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
-  return (
-    <div>
+import React from 'react';
 
-    </div>
-  )
+
+
+export default function Home() {
+
+    console.log('hellow word');
+    
+
+    return <div className="-py-3 my-3 -mb-5 mr-3 bg-none py-3 pb-3"></div>;
 }
