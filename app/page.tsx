b2ac9e1e2657f4a7d99b93174e6aa077adcd9c57
RezaSiahi <rezasiahi8@gmail.com>
2023-12-03 11:34:50 +0000

    fix: (project)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
+import React from "react";
 
-
-
-export default function Home() {
-
-    console.log('hellow word');
-    
-
-    return <div className="-py-3 my-3 -mb-5 mr-3 bg-none py-3 pb-3"></div>;
+export default async function Home() {
+  return <div></div>;
 }
