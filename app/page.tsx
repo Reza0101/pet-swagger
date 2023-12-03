f2c6336f3952a9dc078083fd397e36f47a5a7d04
RezaSiahi <rezasiahi8@gmail.com>
2023-12-03 11:39:33 +0000

    feat: get all pet and show dom

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+import { Pet } from "@/types/api";
+import Link from "next/link";
 import React from "react";
 
+async function fetchData() {
+  const response = await fetch(
+    "https://petstore.swagger.io/v2/pet/findByStatus?status=available",
+  );
+  return response.json();
+}
+
 export default async function Home() {
-  return <div></div>;
+  const data = await fetchData();
+
+
+  return (
+    <div>
+      <div className="w-full bg-green-400 py-3 text-center">
+        <button className="text-lg font-[500]">All pet😎</button>
+      </div>
+
+      <div className="flex flex-col items-center gap-5">
+        {data.map((item: Pet) => (
+          <Link href={`/pet/${item.id}`} className="bg-slate-300 cursor-pointer w-full text-center py-2" key={crypto.randomUUID()}>
+            <p>{item.name}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
